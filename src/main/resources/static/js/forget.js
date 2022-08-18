be5bc5fb0ea16cca48ce828cbeb84ef5a99dd7ff
darksiders1594 <darksiders1594@gmail.com>
2022-08-18 18:02:05 +0800

    重构密码修改逻辑

--- src/main/resources/static/js/forget.js
+++ src/main/resources/static/js/forget.js
@@ -1,5 +1,6 @@
 $(function(){
 	$("#verifyCodeBtn").click(getVerifyCode);
+	$("form").submit(check_data);
 });
 
 function getVerifyCode() {
@@ -9,6 +10,12 @@ function getVerifyCode() {
         alert("还没有填写邮箱哦！");
         return false;
     }
+
+	let check = '^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\\.[a-zA-Z0-9_-]+)+$';
+	if (!email.match(check) && email!="") {
+		alert("邮箱格式有问题哦");
+		return false;
+	}
 
 	$.get(
 	    CONTEXT_PATH + "/forget/code",
@@ -34,4 +41,9 @@ function time(o,wait) {
 			},
 			1000)
 	}
+}
+
+function check_data() {
+	$("#submitBtn").css("pointer-events","none")
+	return true;
 }
